b475b26a8036c6186db02271709fd96bf81864be
Ting-Yi-Lin <39295006+Ting-Yi-Lin@users.noreply.github.com>
2018-06-12 13:30:00 +0800

    Add files via upload

--- main.java
+++ main.java
@@ -1,39 +1,19 @@
 package menu;
 
-import java.io.FileInputStream;
-
-
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
-import javazoom.jl.player.Player;
-
-
-
 public class main {
-	public static FileInputStream fileinputstream;
-	public static menu a;
 
 	public static void main(String[] args) {
-		show();
-		try {
-			fileinputstream = new  FileInputStream("resources/audio/gwyn.mp3");
-			Player player = new Player(fileinputstream);
-			while(true){
-			player.play();
-			System.out.println("Song is playing...");
-			}
-	    } catch(Exception ex) {
-	        System.out.println("Error with playing sound.");
-	        ex.printStackTrace();
-	    }
-	}
-	
-	public static void show(){
-		a = new menu();
+		
+		menu a = new menu();
 		a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		a.setSize(12*26+20, 26*23+50);
 		a.add();
 		a.setVisible(true);
+		
 	}
 
 }
